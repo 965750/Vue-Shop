42b074db73d1e4f5e2a2b24b66f53e264196514d
965750 <aventhelast@gmail.com>
2018-02-06 15:36:59 +0100

    counters on card added

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,9 @@ new Vue({
     el: '#vue-app',
     data: {
         buy: true,
+        empty: true,
+        fullCost: 0,
+        cardCount: 0,
         products: [
             {
                 name: 'apple',
@@ -29,29 +32,31 @@ new Vue({
     methods: {
         readRefs: function () {
             
-            console.log(this.$refs.npInput.value.length);
             
-            if (this.$refs.npInput.value.length < 2 || this.$refs.npInput.value.length > 20) {
+            if (this.$refs.npInput.value.length < 2 || this.$refs.npInput.value.length > 14) {
                 
-                document.getElementById("errOut").innerHTML = 'Name of product must be 2 to 20 characters long.';
+                document.getElementById("errOut").innerHTML = 'Name of product must be 2 to 14 characters long.';
                 
             } else {
-                if (this.$refs.dpInput.value.length <= 5 || this.$refs.dpInput.value.length >= 30) {
+                if (this.$refs.dpInput.value.length <= 4 || this.$refs.dpInput.value.length >= 31) {
                     
                     document.getElementById("errOut").innerHTML = 'Description of product must be 5 to 30 characters long.';
                     
                 } else {
+                    
                     if (this.$refs.aInput.value < 0.5 || this.$refs.aInput.value > 10) {
                         document.getElementById("errOut").innerHTML = 'Price must be between 0.5PLN and 10PLN.';
                         
                     } else {
+                        
                         if (this.$refs.imgInput.value == 0) {
                             this.products.push({
                                 name: this.$refs.npInput.value,
                                 desc: this.$refs.dpInput.value,
                                 cost: this.$refs.aInput.value,
                                 img: "http://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/11/26/1/FNK_rainbow-spectrum_s4x3.jpg.rend.hgtvcom.616.462.suffix/1386172541852.jpeg"
-                            })
+                            });
+                    
                         } else {
                             this.products.push({
                                 name: this.$refs.npInput.value,
@@ -67,11 +72,30 @@ new Vue({
         },
         checkPrice: function(index){
             
+            this.cardCount += 1;
+            this.empty = false;
+            this.fullCost = this.fullCost + this.products[index].cost;
+            this.fullCost = Math.round(this.fullCost * 100) / 100;
+            //(document).getElementById(oneUp).classList.add(plusOne);
+            
             this.card.push({
                 index: index,
                 price: this.products[index].cost,
                 name: this.products[index].name
             });
+        },
+        deleteProduct: function(index){
+            
+            this.cardCount -= 1;
+            
+            this.fullCost = this.fullCost - this.card[index].price;
+            this.fullCost = Math.round(this.fullCost * 100) / 100;
+            this.card.splice(index, 1);
+            
+            if(this.cardCount == 0){
+                this.empty = true;
+                this.fullCost = 0;
+            }
         }
     },
     computed: {
